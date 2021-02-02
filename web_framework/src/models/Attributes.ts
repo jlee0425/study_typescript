dcc5bff72b73cd2d972ca3a0467badee37d46269
jlee0425 <jake.jklee425@gmail.com>
2021-02-02 20:22:03 +0900

    inheritance

---
+++ web_framework/src/models/Attributes.ts
@@ -0,0 +1,33 @@
+import { UserProps } from './User';
+
+export class Attributes<T> {
+	constructor(private props: T) {}
+
+	get<K extends keyof T>(key: K): T[K] {
+		return this.props[key];
+	}
+
+	set(update: T): void {
+		Object.assign(this.props, update);
+	}
+}
+
+const attrs = new Attributes<UserProps>({ id: 5, name: 'asdf', age: 20 });
+const name = attrs.get('name'); // string
+const age = attrs.get('age'); // number
+const id = attrs.get('id'); // number
+
+/**
+ * In JS(TS), object keys are strings.
+ *
+ * when `get` returns string | number(possible types of values of UserProps),
+ *
+ * option 1: type assertion
+ * not desirable as it is easy to miss asserting the type everytime.
+ *
+ * option 2: type-guard
+ * not desirable for the same reason as option 1.
+ *
+ * option 3: inheritance
+ * best option
+ */
--- web_framework/src/models/User.ts
+++ web_framework/src/models/User.ts
@@ -12,13 +12,4 @@ const rootUrl = 'http://localhost:3000/users';
 export class User {
 	events: Events = new Events();
 	sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
-	constructor(private props: UserProps) {}
-
-	get(propName: string): string | number {
-		return this.props[propName];
-	}
-
-	set(update: UserProps): void {
-		Object.assign(this.props, update);
-	}
 }
