7224928168c5a52c9a66e580f7571ec6f9adbe36
jlee0425 <jake.jklee425@gmail.com>
2021-02-08 09:36:37 +0900

    type override

--- express/src/routes/loginRoutes.ts
+++ express/src/routes/loginRoutes.ts
@@ -1,5 +1,9 @@
 import { Router, Request, Response } from 'express';
 
+interface RequestWithBody extends Request {
+	body: { [key: string]: string | undefined };
+}
+
 const router = Router();
 
 router.get('/login', (req: Request, res: Response) => {
@@ -18,13 +22,13 @@ router.get('/login', (req: Request, res: Response) => {
 	`);
 });
 
-router.post('/login', (req: Request, res: Response) => {
+router.post('/login', (req: RequestWithBody, res: Response) => {
 	const { email, password } = req.body;
 
 	if (email) {
-		res.send(email + password);
+		res.send(email.toUpperCase());
 	} else {
-		res.send('You must provide an email and password');
+		res.send('You must provide an email');
 	}
 });
 
